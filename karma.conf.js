269fa730cc843157cdfdbc99d5eac1976f586381
David Young <tgs.wifi@gmail.com>
2017-06-29 18:55:26 +1000

    Chai test now working in both Browser.debug and command line

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
     plugins: [
       'karma-mocha',
       'karma-mocha-reporter',
+      'karma-chai',
       'karma-typescript',
       'karma-chrome-launcher',
       'karma-typescript-preprocessor',
@@ -19,11 +20,11 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'karma-typescript'],
+    frameworks: ['mocha', 'karma-typescript', 'chai'],
 
     // list of files / patterns to load in the browser
     files: [
-      'src/*.ts'
+      'test/*.js', 'test/*.spec.ts', 'src/*.ts', 'test/*.ts'
     ],
 
     // list of files to exclude
@@ -34,7 +35,7 @@ module.exports = function (config) {
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'src/*.ts': ['karma-typescript', 'coverage'],
-      'test/*.ts': ['karma-typescript']
+      'test/*.spec.ts': ['karma-typescript']
     },
 
     // test results reporter to use
@@ -52,27 +53,35 @@ module.exports = function (config) {
       // options passed to the typescript compiler
       options: {
         sourceMap: false, // (optional) Generates corresponding .map file.
-        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
-        module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd'
+        target: 'ES3', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
+        module: 'commonjs', // (optional) Specify module code generation: 'commonjs' or 'amd'
         noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
         noResolve: true, // (optional) Skip resolution and preprocessing.
         removeComments: true, // (optional) Do not emit comments to output.
         concatenateOutput: false, // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
         /* Strict Type-Checking Options */
-        strict: true,                            /* Enable all strict type-checking options. */
-        strictNullChecks: true,              /* Enable strict null checks. */
-        noImplicitThis: true,                /* Raise error on 'this' expressions with an implied 'any' type. */
-        alwaysStrict: true,                  /* Parse in strict mode and emit "use strict" for each source file. */
-        /* Additional Checks */
-        noUnusedLocals: true,                /* Report errors on unused locals. */
-        noUnusedParameters: true,            /* Report errors on unused parameters. */
-        noImplicitReturns: true,             /* Report error when not all code paths in function return a value. */
-        noFallthroughCasesInSwitch: true,    /* Report errors for fallthrough cases in switch statement. */
-        types: ['mocha']
+        // strict: false,                            /* Enable all strict type-checking options. */
+        // strictNullChecks: true,              /* Enable strict null checks. */
+        // noImplicitThis: true,                /* Raise error on 'this' expressions with an implied 'any' type. */
+        // alwaysStrict: true,                  /* Parse in strict mode and emit "use strict" for each source file. */
+        // /* Additional Checks */
+        // noUnusedLocals: true,                /* Report errors on unused locals. */
+        // noUnusedParameters: true,            /* Report errors on unused parameters. */
+        // noImplicitReturns: true,             /* Report error when not all code paths in function return a value. */
+        // noFallthroughCasesInSwitch: true,    /* Report errors for fallthrough cases in switch statement. */
+        types: ['mocha', 'chai']
       },
       // transforming the filenames
       transformPath: function (path) {
         return path.replace(/\.ts$/, '.js')
+      }
+    },
+
+    client: {
+      mocha: {
+        reporter: 'html', // change Karma's debug.html to the mocha web reporter
+        ui: 'bdd'
+       //  expose: ['body'] // This will be exposed in a reporter as `result.mocha.body`
       }
     },
 
--- test/sample.js
+++ test/sample.js
@@ -1,11 +1,7 @@
-import { expect } from 'chai'
-
 describe('A test suite', function () {
   beforeEach(function () { })
   afterEach(function () { })
   it('should fail', function () { expect(true).to.be.false; })
-})
 
-describe('A rogue suite', function () {
-  it('should fail', function () { expect(true).to.be.false; })
+  it('should fail again', function () { expect(true).to.be.false; })
 })
